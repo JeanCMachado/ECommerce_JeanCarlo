c04fb660215153461b05f78231f6bf756462b26f
Jean Carlo Machado de Oliveira <1458095@sga.pucminas.br>
2023-06-14 20:33:02 -0300

    Desenvolvendo

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function init() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        let str = ''
+        for (let i = 0; i < data.length; i++) {
+          let product = data[i]
+          console.log(product)
+          str +=
+            `<div class="row fluid" >          
+              <div class="col-2">
+                  <img src="${product.image}"  alt="..." class="img-thumbnail" style="max-width:100px;">
+                </div>  
+              <div class="col-2">
+                  <p>${product.title}</p>
+                </div>
+                
+                <div class="col-2">
+                  <a class="btn btn-primary bg-danger border-danger" href="detalhes.html?id=${product.id}" role="button">detalhes</a>
+                </div>
+              </div>`
+        }
+        console.log(str)
+        $('#tela').html(str)
+  
+      })
+      .catch(error => console.log(error));
+  
+  }
